3d9bed66fc04aa9037c77230ea12c3d8e202b05c
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2022-09-06 09:51:30 +0100

    add color theme functionality to all pages

--- index.js
+++ index.js
@@ -8,27 +8,30 @@ const pickName = (theName) => {
     const faviconTag = document.getElementById("favicon");
     if (theName === "dark-plus") {
         colorName.innerHTML = "Dark+";
-        faviconTag.setAttribute("href", "./images/favicon-dark-plus.png");
+        faviconTag.setAttribute("href", "../images/favicon-dark-plus.png");
     }
     if (theName === "light-plus") {
         colorName.innerHTML = "Light+";
-        faviconTag.setAttribute("href", "./images/favicon-light-plus (1).png");
+        faviconTag.setAttribute("href", "../images/favicon-light-plus (1).png");
     }
     if (theName === "monokai") {
         colorName.innerHTML = "Monokai";
-        faviconTag.setAttribute("href", "./images/favicon-monokai.png");
+        faviconTag.setAttribute("href", "../images/favicon-monokai.png");
     }
     if (theName === "github-dark") {
         colorName.innerHTML = "Github Dark";
-        faviconTag.setAttribute("href", "./images/favicon-github-dark.png");
+        faviconTag.setAttribute("href", "../images/favicon-github-dark.png");
     }
     if (theName === "github-light") {
         colorName.innerHTML = "Github Light";
-        faviconTag.setAttribute("href", "./images/favicon-github-light.png");
+        faviconTag.setAttribute("href", "../images/favicon-github-light.png");
     }
     if (theName === "solarized-light") {
         colorName.innerHTML = "Solarized Light";
-        faviconTag.setAttribute("href", "./images/favicon-solarized-light.png");
+        faviconTag.setAttribute(
+            "href",
+            "../images/favicon-solarized-light.png"
+        );
     }
     localStorage.setItem("colorName", colorName.innerHTML);
     localStorage.setItem("favicon-tag", faviconTag.getAttribute("href"));
